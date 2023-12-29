c335908b8c6d45f79518bef037dcb7e2b26f9f81
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-12-29 22:48:59 +0300

    2

--- src/routes/(loadUkazania)/+page.svelte
+++ src/routes/(loadUkazania)/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	let date: string = new Date().toISOString().slice(0, 10);
+	$: prevDate = () => {
+		let date1 = new Date(date);
+		date1.setTime(date1.getTime() - 24 * 60 * 60 * 1000);
+		return date1.toISOString().slice(0, 10);
+	};
+	$: nextDate = () => {
+		let date1 = new Date(date);
+		date1.setTime(date1.getTime() + 24 * 60 * 60 * 1000);
+		return date1.toISOString().slice(0, 10);
+	};
 	$: url = `http://www.patriarchia.ru/bu/${date}/print.html`;
 	$: getSite = async () => {
 		let result = await fetch('/api/url', {
@@ -18,14 +28,26 @@
 <div class="sticky-top bg-dark text-light">
 	<div class="container">
 		<div class="d-flex align-items-center gap-3 mb-2 px-1 py-2">
-			<b class="flex-grow-1 mb-0 textLf yt pf xnj? c yfcnegf.obvb-dark-subtle text-nowrap"
-				>Изменить дату</b
-			>
+			<b class="flex-grow-1 mb-0 text-dark-subtle text-nowrap">Изменить дату</b>
 			<div class="input-group">
 				<input class="form-control border-0 bg-dark-subtle" type="date" bind:value={date} />
-				<button
+				<a
+					href="/"
+					title="Сегодня"
 					class="btn btn-outline-light"
-					on:click={() => (date = new Date().toISOString().slice(0, 10))}>Сегодня</button
+					on:click={() => (date = new Date().toISOString().slice(0, 10))}>Сегодня</a
+				>
+				<a
+					href="/"
+					title="Предыдущий день"
+					class="btn btn-outline-light"
+					on:click={() => (date = prevDate())}><i class="fa-solid fa-arrow-left"></i></a
+				>
+				<a
+					href="/"
+					title="Следующий день"
+					class="btn btn-outline-light"
+					on:click={() => (date = nextDate())}><i class="fa-solid fa-arrow-right"></i></a
 				>
 			</div>
 		</div>
