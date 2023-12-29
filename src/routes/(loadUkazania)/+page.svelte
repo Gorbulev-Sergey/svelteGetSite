f16eef3af408def51b444cd00ac3d80f760a52b9
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-12-29 23:13:49 +0300

    4

--- src/routes/(loadUkazania)/+page.svelte
+++ src/routes/(loadUkazania)/+page.svelte
@@ -27,27 +27,27 @@
 
 <div class="sticky-top bg-dark text-light">
 	<div class="container">
-		<div class="d-flex align-items-center gap-3 mb-2 px-1 py-2">
-			<b class="flex-grow-1 mb-0 text-dark-subtle text-nowrap">Выбрать дату:</b>
-			<div class="input-group">
-				<input class="form-control border-0 bg-dark-subtle" type="date" bind:value={date} />
-				<a
-					href="/"
-					title="Сегодня"
-					class="btn btn-outline-light"
-					on:click={() => (date = new Date().toISOString().slice(0, 10))}>Сегодня</a
-				>
+		<div class="d-flex flex-wrap align-items-center gap-1 mb-2 px-1 py-2">
+			<div class="d-flex align-items-center gap-2 mb-1 mb-md-0">
+				<b class="flex-grow-1 mb-0 text-light text-nowrap">Выбрать дату:</b>
+			</div>
+			<div class="d-flex align-items-center gap-1">
 				<a
 					href="/"
 					title="Предыдущий день"
-					class="btn btn-outline-light"
+					class="btn btn-dark"
 					on:click={() => (date = prevDate())}><i class="fa-solid fa-arrow-left"></i></a
 				>
+				<input class="form-control border-0 bg-light" type="date" bind:value={date} />
 				<a
 					href="/"
-					title="Следующий день"
-					class="btn btn-outline-light"
-					on:click={() => (date = nextDate())}><i class="fa-solid fa-arrow-right"></i></a
+					title="Сегодня"
+					class="btn btn-dark"
+					on:click={() => (date = new Date().toISOString().slice(0, 10))}>Сегодня</a
+				>
+
+				<a href="/" title="Следующий день" class="btn btn-dark" on:click={() => (date = nextDate())}
+					><i class="fa-solid fa-arrow-right"></i></a
 				>
 			</div>
 		</div>
