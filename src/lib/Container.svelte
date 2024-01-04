6eee65ee7f36beae6f76cf2ce8caf851f069768e
Горбулёв Сергей <gorbulevsv@yandex.ru>
2024-01-04 11:11:23 +0300

    Изменено оформление

---
+++ src/lib/Container.svelte
@@ -0,0 +1,3 @@
+<div class="bg-white container p-4 mt-2 mb-5 rounded-3">
+	<slot />
+</div>
--- src/routes/(loadUkazania)/+page.svelte
+++ src/routes/(loadUkazania)/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Container from '$lib/Container.svelte';
+
 	let date: string = new Date().toISOString().slice(0, 10);
 	$: prevDate = () => {
 		let date1 = new Date(date);
@@ -25,29 +27,32 @@
 	<title>Богослужебные указания</title>
 </svelte:head>
 
-<div class="sticky-top bg-dark text-light">
+<div class="sticky-top bg-light text-dark">
 	<div class="container">
-		<div class="d-flex flex-wrap align-items-center gap-1 mb-2 px-1 py-2">
+		<div class="d-flex flex-wrap align-items-center justify-content-center gap-1 px-1 py-3">
 			<div class="d-flex align-items-center gap-2 mb-1 mb-md-0">
-				<b class="flex-grow-1 mb-0 text-light text-nowrap">Выбрать дату:</b>
+				<b class="flex-grow-1 mb-0 text-nowrap">Выбрать дату:</b>
 			</div>
 			<div class="d-flex align-items-center gap-1">
 				<a
 					href="/"
 					title="Предыдущий день"
-					class="btn btn-dark"
+					class="btn btn-light"
 					on:click={() => (date = prevDate())}><i class="fa-solid fa-arrow-left"></i></a
 				>
 				<input class="form-control border-0 bg-light" type="date" bind:value={date} />
 				<a
 					href="/"
 					title="Сегодня"
-					class="btn btn-dark"
+					class="btn btn-light"
 					on:click={() => (date = new Date().toISOString().slice(0, 10))}>Сегодня</a
 				>
 
-				<a href="/" title="Следующий день" class="btn btn-dark" on:click={() => (date = nextDate())}
-					><i class="fa-solid fa-arrow-right"></i></a
+				<a
+					href="/"
+					title="Следующий день"
+					class="btn btn-light"
+					on:click={() => (date = nextDate())}><i class="fa-solid fa-arrow-right"></i></a
 				>
 			</div>
 		</div>
@@ -55,10 +60,9 @@
 </div>
 
 {#await getSite() then result}
-	<div
-		class="container mt-4 mb-5 text-dark rounded"
-		style="font-size:1.2em; line-height: 1.4em; font-weight: 400;"
-	>
-		{@html result?.replace('Богослужебные указания за', 'Богослужебные указания на')}
-	</div>
+	<Container>
+		<div style="font-size:1.2em; line-height: 1.4em; font-weight: 400;">
+			{@html result?.replace('Богослужебные указания за', 'Богослужебные указания на')}
+		</div>
+	</Container>
 {/await}
