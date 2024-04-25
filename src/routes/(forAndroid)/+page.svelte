1549ddefe4d3c049a46c9ec3c86882af041507cf
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-04-25 23:10:04 +0300

    5

--- src/routes/(forAndroid)/+page.svelte
+++ src/routes/(forAndroid)/+page.svelte
@@ -1,5 +1,9 @@
-<script>
-	export let message = 'Привет';
+<script lang="ts">
+	export let message: string = 'Привет';
+
+	export let setMessage = (m: string) => {
+		message = m;
+	};
 </script>
 
 <h1 class="text-light m-5">{message}</h1>
