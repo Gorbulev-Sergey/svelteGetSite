7c1efd68c38a09a7f8c5ba56b1a8dc74d7cdf3c4
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-09-27 16:33:14 +0300

    2

--- src/routes/api/url/+server.ts
+++ src/routes/api/url/+server.ts
@@ -2,10 +2,8 @@ export async function POST({ request, fetch }) {
 	let { url } = await request.json();
 	let result = await fetch(url, {
 		method: 'GET',
-		mode: 'no-cors',
 		headers: {
-			'Content-Type': 'text/html; charset=utf-8',
-			'Access-Control-Allow-Origin': '*'
+			'Content-Type': 'text/html; charset=utf-8'
 		}
 	});
 	return new Response(await result.text());
