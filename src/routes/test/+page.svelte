4b015c4cf7005ac5a4fa2076650dd31369e2611a
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-12-23 21:32:53 +0300

    test

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export function setColor() {
+		let e = document.querySelector('#text');
+		e.style.backgroundColor = 'blue';
+	}
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div id="text" on:click={() => setColor()}>Привет</div>
